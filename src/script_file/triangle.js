e32cf29f6f5eccf046877a07f47d2432fd2de376
Md. Abdullah Al Hasan <hasan.cse.iub@gmail.com>
2024-11-21 02:23:24 +0600

    kkk

---
+++ src/script_file/triangle.js
@@ -0,0 +1,12 @@
+const trinagle = document.getElementById('traingleCalculation').addEventListener('click',function(){
+
+    let b = document.getElementById('inputB');
+    let h = document.getElementById('inputH');
+    let cem = document.getElementById('cem');
+    let result = 0.5 * b.value * h.value;
+    let  resultShow = document.getElementById('show-result');
+    cem.hidden = false;
+
+    resultShow.innerText=result;
+
+})
