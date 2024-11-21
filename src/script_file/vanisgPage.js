4c6eba3ca2ecd2a97f42dc6cb6c55292dce99925
Md. Abdullah Al Hasan <hasan.cse.iub@gmail.com>
2024-11-21 20:21:42 +0600

    pppp

--- src/script_file/vanisgPage.js
+++ src/script_file/vanisgPage.js
@@ -3,8 +3,30 @@ function PlayGame() {
     const page = showpage('letter');  // Get the page element with ID 'letter'
     
      // The 'page' element is already referenced, no need to assign again.
-    page.innerText = generateAlphabet.toUpperCase();  // Set the text to the generated letter in uppercase
+
+     const pageDown  = page.innerText = generateAlphabet.toUpperCase();  // Set the text to the generated letter in uppercase
 }
+
+ function handleKeyboardButtonPress(event){
+    const playerPressed = event.key;
+   const currentShow = document.getElementById('letter');
+   const currentShowPressed = currentShow.innerText;
+  
+
+   if (currentShowPressed==playerPressed){
+    alert('Presed right')
+   }
+   else{
+    alert("Wrong Key Pressed")
+   }
+   console.log(currentShowPressed);
+ console.log('Player Pressed: '+playerPressed) ;
+
+
+}
+
+document.addEventListener('keyup', handleKeyboardButtonPress) 
+ console.log ('Button Pressed') ;
 
 function RandomAlphabet() {
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';  // Alphabet string
@@ -13,9 +35,10 @@ function RandomAlphabet() {
     const randomNumber = Math.random() * 26;  // Random number between 0 and 25
     const index = Math.floor(randomNumber);  // Get an integer index from 0 to 25
     
-    console.log('Generated index: ' + index);  // Debugging the index
+    //console.log('Generated index: ' + index);  // Debugging the index
     
-    const alphabetAndIndex = alphabets[index];  // Get the alphabet at the random index
+    const alphabetAndIndex = alphabets[index];  
+     console.log('Generated Alphabet: ' + alphabetAndIndex + ' -> Alphabets Index: '+ index);  //// Get the alphabet at the random indexs
     return alphabetAndIndex;  // Return the letter
 }
 
